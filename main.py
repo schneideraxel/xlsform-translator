03ddd00a51af421dcb00c9b35cbdabcd8cddaf9a
schneideraxel <axel.schneider@ik.me>
2026-04-05 23:00:31 -0400

    entry point header

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+### Entry point : run this file directly with Python to invoke the CLI
+### AS 🐚🫧🪼🪸
+### 05.04.2026 (Last update)
+
 from xlsform_translator.cli import run
 
 if __name__ == "__main__":
